d21250eabd63f499b742c626c75088a65f0f31b0
anonymous anonymous <ashnavbhatia@gmail.com>
2022-03-11 22:38:49 +0000

    Direct association between brand_names and clothing_pieces

--- spec/models/brand_name_spec.rb
+++ spec/models/brand_name_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BrandName, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:clothing_pieces) }
+
     end
 
     describe "InDirect Associations" do
