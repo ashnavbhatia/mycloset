5158bfda39489972d63058143cf5fa529b30a622
anonymous anonymous <ashnavbhatia@gmail.com>
2022-03-11 22:39:41 +0000

    Generate graphiti resources for borrowers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
     scope module: 'api/v1', as: 'api' do
+      resources :borrowers
+
       resources :clothing_type_ids
 
       resources :brand_names
